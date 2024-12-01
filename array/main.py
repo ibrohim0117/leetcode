4b4dc6be70c09678f3239984f4960af04b1853b1
ibrohim <ibrohim.dev.uz@gmail.com>
2024-12-02 00:04:47 +0500

    .

---
+++ array/main.py
@@ -0,0 +1,20 @@
+# 136. Single Number
+# nums = [4]
+# for i in nums:
+#     a = nums.count(i)
+#     if a == 1:
+#         print(i)
+#         break         # done
+
+
+
+# 169. Majority Element
+# nums = [2,2,1,1,1,2,2]
+# for i in nums:
+#     if nums.count(i) >= len(nums)/2:
+#         print(i)
+#         break               # no done
+
+
+
+
