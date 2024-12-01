93071047e858da4d3e9a50be22d4131c5356c300
ibrohim <ibrohim.dev.uz@gmail.com>
2024-12-02 00:16:29 +0500

    .

---
+++ string/main.py
@@ -0,0 +1,14 @@
+# 125. Valid Palindrome
+# s = "0P"
+# new = ''
+# for i in s:
+#     if i.isalnum():
+#         print(i)
+#         if i.isupper():
+#             new += i.lower()
+#         else:
+#             new += i
+# if new == new[::-1]:
+#     print(True)
+# else:
+#     print(False)            # done
